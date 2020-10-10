cc72c2f99eea5a7ace81fc14f2cb865dcab6c2f0
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-10 11:18:34 +0100

    Plugin companion object for spigot

--- mercury-spigot/src/main/java/uk/cmdrnorthpaw/mercury/MercurySpigot.kt
+++ mercury-spigot/src/main/java/uk/cmdrnorthpaw/mercury/MercurySpigot.kt
@@ -1,13 +1,18 @@
 package uk.cmdrnorthpaw.mercury;
 
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin;
 
-object MercurySpigot : JavaPlugin() {
+class MercurySpigot : JavaPlugin() {
     override fun onEnable() {
 
     }
 
     override fun onDisable() {
 
+    }
+    companion object {
+        val plugin: Plugin
+        get() = getPlugin(MercurySpigot::class.java)
     }
 }
