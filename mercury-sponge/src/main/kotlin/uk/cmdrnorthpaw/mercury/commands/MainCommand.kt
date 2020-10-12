56d3fd8ae9487a68a8c94c171e24f9213a15c53a
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-12 19:34:57 +0100

    Started on a Sponge command so I can test this thing

---
+++ mercury-sponge/src/main/kotlin/uk/cmdrnorthpaw/mercury/commands/MainCommand.kt
@@ -0,0 +1,12 @@
+package uk.cmdrnorthpaw.mercury.commands
+
+import org.spongepowered.api.command.CommandResult
+import org.spongepowered.api.command.CommandSource
+import org.spongepowered.api.command.args.CommandContext
+import org.spongepowered.api.command.spec.CommandExecutor
+
+object MainCommand : CommandExecutor {
+    override fun execute(src: CommandSource, args: CommandContext): CommandResult {
+        TODO("Need to finish up the install function before writing this")
+    }
+}
