560410777ceecbd0e2784d7cfa22b16b6c63d398
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-12 08:59:34 +0100

    Made a start on handling the Ore web API

---
+++ mercury-common/src/main/kotlin/apis/ore/OreFile.kt
@@ -0,0 +1,20 @@
+package apis.ore
+
+import com.beust.klaxon.Json
+
+data class OreFile(
+    @Json(name = "id") val recommendedId: Int,
+    val name: String,
+    val dependencies: List<OreDependency>,
+    val pluginId: String,
+    val fileSize: Int,
+    val md5: String,
+    @Json(name = "staffApproved") val approved: Boolean,
+    val href: String
+
+)
+
+data class OreDependency(
+        @Json(name = "pluginId") val id: String,
+        val version: String
+)
