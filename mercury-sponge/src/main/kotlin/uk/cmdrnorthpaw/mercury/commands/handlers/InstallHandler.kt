d5518583d6a92d86dd603bd3fb507a25d7a4aecf
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-16 15:21:31 +0100

    Update uses of plugin.download() and run downloads in non-blocking fashion

--- mercury-sponge/src/main/kotlin/uk/cmdrnorthpaw/mercury/commands/handlers/InstallHandler.kt
+++ mercury-sponge/src/main/kotlin/uk/cmdrnorthpaw/mercury/commands/handlers/InstallHandler.kt
@@ -1,13 +1,17 @@
 package uk.cmdrnorthpaw.mercury.commands.handlers
 
 import apis.ore.OreResource
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
 import org.spongepowered.api.command.args.CommandContext
 import org.spongepowered.api.text.Text
 import org.spongepowered.api.text.format.TextColor
 import org.spongepowered.api.text.format.TextColors
+import java.io.File
 
 fun install(src: CommandSource, args: CommandContext): CommandResult {
     val pluginId = args.getOne<String>("plugin")
@@ -18,6 +22,8 @@ fun install(src: CommandSource, args: CommandContext): CommandResult {
     val plugin = runBlocking { OreResource.get(pluginId.get()) }
     val version = args.getOne<Float>("version")
 
-    runBlocking { if (version.isPresent) plugin?.download() else plugin?.download(version.get()) }
+    val targetFile = File(Sponge.getGame().gameDirectory.toFile(), "/mods/${plugin?.id}")
+
+    GlobalScope.launch { if (version.isPresent) plugin?.download(targetFile) else plugin?.download(targetFile, version.get()) }
     return CommandResult.success()
 }
