222ff5c1eb68299087925ca58bddc575a322f0c9
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-17 13:54:55 +0100

    Add some config management stuff

--- mercury-sponge/src/main/kotlin/uk/cmdrnorthpaw/mercury/MercurySponge.kt
+++ mercury-sponge/src/main/kotlin/uk/cmdrnorthpaw/mercury/MercurySponge.kt
@@ -1,10 +1,13 @@
 package uk.cmdrnorthpaw.mercury;
 
-import com.google.inject.Inject;
-import org.slf4j.Logger;
-import org.spongepowered.api.event.game.state.GameStartedServerEvent;
-import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.plugin.Plugin;
+import com.google.inject.Inject
+import ninja.leaping.configurate.commented.CommentedConfigurationNode
+import ninja.leaping.configurate.loader.ConfigurationLoader
+import org.spongepowered.api.config.DefaultConfig
+import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.game.state.GameStartedServerEvent
+import org.spongepowered.api.plugin.Plugin
+import java.nio.file.Path
 
 @Plugin(
         id = "mercury-sponge",
@@ -19,5 +22,14 @@ object MercurySponge {
     fun onEnable(event: GameStartedServerEvent) {
 
     }
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private val configFile: Path? = null
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private val configNode: ConfigurationLoader<CommentedConfigurationNode>? = null
+
 }
 
